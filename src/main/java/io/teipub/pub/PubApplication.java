d554f5514f534238a17ef8ef2dc5253d454fe2b2
tei <takesoft@gmail.com>
2017-03-14 10:06:36 +0900

    change dir structure

--- src/main/java/io/teipub/pub/PubApplication.java
+++ src/main/java/io/teipub/pub/PubApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PubApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PubApplication.class, args);
 	}
 }
